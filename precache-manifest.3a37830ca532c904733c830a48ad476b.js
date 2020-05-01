7189023253c46807ea1532845855494bb7e8aa39
javier <javier@pc-ljm.fi.unam>
2020-05-01 12:49:33 -0500

    Updates

---
+++ precache-manifest.3a37830ca532c904733c830a48ad476b.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a96d0e38cb0555d59bd15eaecd6e043e",
+    "url": "/DiplimadoReact/index.html"
+  },
+  {
+    "revision": "8b32a47dd8f9222cafc9",
+    "url": "/DiplimadoReact/static/css/main.fae562b0.chunk.css"
+  },
+  {
+    "revision": "155ce603c0b2aab04e14",
+    "url": "/DiplimadoReact/static/js/2.0d355ad3.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/DiplimadoReact/static/js/2.0d355ad3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b32a47dd8f9222cafc9",
+    "url": "/DiplimadoReact/static/js/main.8ac490cf.chunk.js"
+  },
+  {
+    "revision": "d1224e2b33fb8eba9a72",
+    "url": "/DiplimadoReact/static/js/runtime-main.e215c7aa.js"
+  },
+  {
+    "revision": "98052b8dd867261f606703743f326563",
+    "url": "/DiplimadoReact/static/media/Sin titulo-1.98052b8d.svg"
+  },
+  {
+    "revision": "5dbb263d699c42c86f3f8701471fc888",
+    "url": "/DiplimadoReact/static/media/back_apple_002.5dbb263d.png"
+  },
+  {
+    "revision": "435748a9a165938039eb14bed7407876",
+    "url": "/DiplimadoReact/static/media/back_grapes_001.435748a9.png"
+  },
+  {
+    "revision": "f3326ada187300411fe50d46e2b0eb6f",
+    "url": "/DiplimadoReact/static/media/back_strawberry_003.f3326ada.png"
+  },
+  {
+    "revision": "552769b7f48d7beebb71be7de12c2f5b",
+    "url": "/DiplimadoReact/static/media/bottle_apple_002.552769b7.png"
+  },
+  {
+    "revision": "51716f6a0d212bf63e964f3c61dd94ae",
+    "url": "/DiplimadoReact/static/media/bottle_grapes_001.51716f6a.png"
+  },
+  {
+    "revision": "232a73611dc883d99f0dbcdf2707568e",
+    "url": "/DiplimadoReact/static/media/bottle_strawberry_003.232a7361.png"
+  },
+  {
+    "revision": "cc50e3d6cd28a810544843b6d25ec10a",
+    "url": "/DiplimadoReact/static/media/example-image.cc50e3d6.jpg"
+  },
+  {
+    "revision": "6f9c36e9761f6c78c7553f11f5e4d38b",
+    "url": "/DiplimadoReact/static/media/front_apple_002.6f9c36e9.png"
+  },
+  {
+    "revision": "0c27cc78d4f7ed9d47e71e5b76237ca9",
+    "url": "/DiplimadoReact/static/media/front_grapes_001.0c27cc78.png"
+  },
+  {
+    "revision": "fbf1c2e3e9b89f4b8b831029a48eff24",
+    "url": "/DiplimadoReact/static/media/front_strawberry_003.fbf1c2e3.png"
+  },
+  {
+    "revision": "ce07c578efe54bebd8007f8e3fed5c32",
+    "url": "/DiplimadoReact/static/media/imagen.ce07c578.png"
+  }
+]);
